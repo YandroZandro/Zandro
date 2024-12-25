abed99e9027a58856f03949671af2117f7adba30
YandroZandro <zandrojpc@gmail.com>
2024-12-25 20:18:04 +0800

    Delete limoweb.js

--- limoweb.js
+++
@@ -1,6 +0,0 @@
-const toggler = document.querySelector('.navbar-toggler');
-const navbar = document.querySelector('.navbar');
-
-toggler.addEventListener('click', () => {
-  navbar.classList.toggle('active'); // Toggle visibility
-});
